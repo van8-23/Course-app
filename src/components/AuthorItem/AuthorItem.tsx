f2905c1d871227b92f7f2e2be852d16bf67111f9
Vadym <vadimkozka@gmail.com>
2025-11-09 15:25:34 +0200

    still trying

--- src/components/AuthorItem/AuthorItem.tsx
+++ src/components/AuthorItem/AuthorItem.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 import Button from '../../common/Button/Button';
 
 export type AuthorItemProps = {
@@ -23,6 +23,39 @@ const AuthorItem: React.FC<AuthorItemProps> = ({ name, variant, onAdd, onRemove 
     <div className="cc-row">
       <span className="cc-author-name">{name}</span>
       <Button buttonText={isAdd ? 'Add author' : 'Delete author'} onClick={handleClick} type="button" />
+    </div>
+  );
+};
+
+export default AuthorItem;*/
+
+
+import React from 'react';
+import Button from '../../common/Button/Button';
+
+export type AuthorItemProps = {
+  id: string;
+  name: string;
+  isCourseAuthor?: boolean;
+  onButtonClick: (id: string) => void;
+};
+
+const AuthorItem: React.FC<AuthorItemProps> = ({
+  id,
+  name,
+  isCourseAuthor = false,
+  onButtonClick,
+}) => {
+  const buttonText = isCourseAuthor ? 'Delete author' : 'Add author';
+
+  return (
+    <div className="cc-row">
+      <span className="cc-author-name">{name}</span>
+      <Button
+        buttonText={buttonText}
+        type="button"
+        onClick={() => onButtonClick(id)}
+      />
     </div>
   );
 };
