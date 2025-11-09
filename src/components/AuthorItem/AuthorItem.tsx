1401acdbde96307b4f9035824e7428e85b5733fe
Vadym <vadimkozka@gmail.com>
2025-11-09 16:17:51 +0200

    one more trying

--- src/components/AuthorItem/AuthorItem.tsx
+++ src/components/AuthorItem/AuthorItem.tsx
@@ -1,37 +1,4 @@
-
-
 /*
-import React from 'react';
-import Button from '../../common/Button/Button';
-
-export type AuthorItemProps = {
-  id: string;
-  name: string;
-  isCourseAuthor?: boolean;
-  onButtonClick: (id: string) => void;
-};
-
-const AuthorItem: React.FC<AuthorItemProps> = ({
-  id,
-  name,
-  isCourseAuthor = false,
-  onButtonClick,
-}) => {
-  const buttonText = isCourseAuthor ? 'Delete author' : 'Add author';
-
-  return (
-    <div className="cc-row">
-      <span className="cc-author-name">{name}</span>
-      <Button
-        buttonText={buttonText}
-        type="button"
-        onClick={() => onButtonClick(id)}
-      />
-    </div>
-  );
-};
-
-export default AuthorItem;*/
 
 import React from 'react';
 import Button from '../../common/Button/Button';
@@ -62,6 +29,38 @@ const AuthorItem: React.FC<AuthorItemProps> = ({
         buttonText={label}
         type="button"
         onClick={handleClick}
+      />
+    </div>
+  );
+};
+
+export default AuthorItem;*/
+
+import React from 'react';
+import Button from '../../common/Button/Button';
+
+export type AuthorItemProps = {
+  id: string;
+  name: string;
+  isCourseAuthor?: boolean;
+  onButtonClick: (id: string) => void;
+};
+
+const AuthorItem: React.FC<AuthorItemProps> = ({
+  id,
+  name,
+  isCourseAuthor = false,
+  onButtonClick,
+}) => {
+  const label = isCourseAuthor ? 'Delete author' : 'Add author';
+
+  return (
+    <div className="cc-row">
+      <span className="cc-author-name">{name}</span>
+      <Button
+        buttonText={label}
+        type="button"
+        onClick={() => onButtonClick(id)}
       />
     </div>
   );
